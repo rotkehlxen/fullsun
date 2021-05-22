16b7793a7835b6f687283e3a13f829176540a0df
Franziska Witzel <franziska.witzel@gmail.com>
2021-05-22 17:45:51 +0200

    obtain values from input fields, manipulate html programmatically

---
+++ js/myscripts.js
@@ -0,0 +1,11 @@
+function printSuncalStatus(){
+	const latitude = document.getElementById("latitude").value
+	const longitude = document.getElementById("longitude").value
+	const fromDate = document.getElementById("from-date").value
+	const toDate = document.getElementById("to-date").value
+
+	const message = `Creating calendar events for location 
+	${latitude}°, ${longitude}° from ${fromDate} to ${toDate}.`
+
+	document.getElementById("statusMessage").textContent = message
+}
