dfd51be40bc92478edac87ce9e20ea713d994e08
Franziska Witzel <franziska.witzel@gmail.com>
2021-05-23 13:16:04 +0200

    validation of end date greater or equal to start date

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,12 +1,32 @@
-function printSuncalStatus(){
-	// deprecated
-	const latitude = document.getElementById("latitude").value
-	const longitude = document.getElementById("longitude").value
-	const fromDate = document.getElementById("from-date").value
-	const toDate = document.getElementById("to-date").value
+function validateFromDate(){
+	const fromDate = new Date(document.getElementById("from-date").value)
+	const toDate = new Date(document.getElementById("to-date").value)
 
-	const message = `Creating calendar events for location 
-	${latitude}°, ${longitude}° from ${fromDate} to ${toDate}.`
+	const fromDateField = document.getElementById("from-date")
 
-	document.getElementById("statusMessage").textContent = message
+	if (!toDate.getTime() || (toDate.getTime() >= fromDate.getTime())) {
+		 fromDateField.setCustomValidity("");
+	}
+	else {
+		fromDateField.setCustomValidity("Start date must be smaller or equal to end date.")
+	}
+}
+
+function validateToDate(){
+	const fromDate = new Date(document.getElementById("from-date").value)
+	const toDate = new Date(document.getElementById("to-date").value)
+
+	const toDateField = document.getElementById("to-date")
+
+	if (!fromDate.getTime() || (toDate.getTime() >= fromDate.getTime())) {
+		 toDateField.setCustomValidity("");
+	}
+	else {
+		toDateField.setCustomValidity("End date must be greater or equal to start date.")
+	}
+}
+
+window.onload = function () {
+    document.getElementById("to-date").oninput = validateToDate;
+    document.getElementById("from-date").oninput = validateFromDate;
 }
